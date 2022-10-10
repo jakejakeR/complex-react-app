9dcca2a6d31957ae5677e0d67bd8ba96bd6cb4ac
Jakub Rogowski <jakerogowski33@gmail.com>
2022-10-10 13:11:05 +0200

    S8L54: Client-Side Form Validation

--- app/components/EditPost.js
+++ app/components/EditPost.js
@@ -6,6 +6,7 @@ import Axios from "axios";
 import LoadingDotsIcon from "./LoadingDotsIcon";
 import StateContext from "../StateContext";
 import DispatchContext from "../DispatchContext";
+import { finishDraft } from "immer";
 
 function ViewSinglePost() {
     const appState = useContext(StateContext);
@@ -36,19 +37,35 @@ function ViewSinglePost() {
                 draft.isFetching = false;
                 return;
             case "titleChange":
+                draft.title.hasErrors = false;
                 draft.title.value = action.value;
                 return;
             case "bodyChange":
+                draft.body.hasErrors = false;
                 draft.body.value = action.value;
                 return;
             case "submitRequest":
-                draft.sendCount++;
+                if (!draft.title.hasErrors && !draft.body.hasErrors) {
+                    draft.sendCount++;
+                }
                 return;
             case "saveRequestsStarted":
                 draft.isSaving = true;
                 return;
             case "saveRequestFinished":
                 draft.isSaving = false;
+                return;
+            case "titleRules":
+                if (!action.value.trim()) {
+                    draft.title.hasErrors = true;
+                    draft.title.message = "You must provide a titile.";
+                }
+                return;
+            case "bodyRules":
+                if (!action.value.trim()) {
+                    draft.body.hasErrors = true;
+                    draft.body.message = "You must provide body content.";
+                }
                 return;
         }
     }
@@ -57,6 +74,8 @@ function ViewSinglePost() {
 
     function submitHandler(e) {
         e.preventDefault();
+        dispatch({ type: "titleRules", value: state.title.value });
+        dispatch({ type: "bodyRules", value: state.body.value });
         dispatch({ type: "submitRequest" });
     }
 
@@ -110,14 +129,16 @@ function ViewSinglePost() {
                     <label htmlFor="post-title" className="text-muted mb-1">
                         <small>Title</small>
                     </label>
-                    <input onChange={e => dispatch({ type: "titleChange", value: e.target.value })} value={state.title.value} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+                    <input onBlur={e => dispatch({ type: "titleRules", value: e.target.value })} onChange={e => dispatch({ type: "titleChange", value: e.target.value })} value={state.title.value} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+                    {state.title.hasErrors && <div className="alert alert-danger small liveValidateMessage">{state.title.message}</div>}
                 </div>
 
                 <div className="form-group">
                     <label htmlfor="post-body" className="text-muted mb-1 d-block">
                         <small>Body Content</small>
                     </label>
-                    <textarea onChange={e => dispatch({ type: "bodyChange", value: e.target.value })} name="body" id="post-body" className="body-content tall-textarea form-control" type="text" value={state.body.value} />
+                    <textarea onBlur={e => dispatch({ type: "bodyRules", value: e.target.value })} onChange={e => dispatch({ type: "bodyChange", value: e.target.value })} name="body" id="post-body" className="body-content tall-textarea form-control" type="text" value={state.body.value} />
+                    {state.body.hasErrors && <div className="alert alert-danger small liveValidateMessage">{state.body.message}</div>}
                 </div>
 
                 <button className="btn btn-primary" disabled={state.isSaving}>
