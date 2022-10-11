aaaa456c8f9468f18e473d7e98f5e69ef34b4118
Jakub Rogowski <jakerogowski33@gmail.com>
2022-10-11 17:41:20 +0200

    S10L65: Homepage Post Feed

--- app/components/ProfilePosts.js
+++ app/components/ProfilePosts.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import Axios from "axios";
 import LoadingDotsIcon from "./LoadingDotsIcon";
+import Post from "./Post";
 
 function ProfilePosts(props) {
     const { username } = useParams();
@@ -36,14 +37,7 @@ function ProfilePosts(props) {
     return (
         <div className="list-group">
             {posts.map(post => {
-                const date = new Date(post.createdDate);
-                const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-
-                return (
-                    <Link key={post._id} to={`/post/${post._id}`} className="list-group-item list-group-item-action">
-                        <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong> <span className="text-muted small">on {dateFormatted} </span>
-                    </Link>
-                );
+                return <Post noAuthor={true} post={post} key={post._id} />;
             })}
         </div>
     );
