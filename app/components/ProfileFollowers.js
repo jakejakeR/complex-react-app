ad2e940cb89e0427d7b17e4b6201d5f53ef6286b
Jakub Rogowski <jakerogowski33@gmail.com>
2022-10-11 16:46:24 +0200

    S10L64: Followers and Following tabs

--- app/components/ProfileFollowers.js
+++ app/components/ProfileFollowers.js
@@ -3,7 +3,7 @@ import { useParams, Link } from "react-router-dom";
 import Axios from "axios";
 import LoadingDotsIcon from "./LoadingDotsIcon";
 
-function ProfilePosts(props) {
+function ProfileFollowers(props) {
     const { username } = useParams();
     const [isLoading, setIsLoading] = useState(true);
     const [posts, setPosts] = useState([]);
@@ -13,7 +13,7 @@ function ProfilePosts(props) {
 
         async function fetchPosts() {
             try {
-                const response = await Axios.get(`/profile/${username}/posts`, { cancelToken: ourRequest.token });
+                const response = await Axios.get(`/profile/${username}/followers`, { cancelToken: ourRequest.token });
                 setPosts(response.data);
                 setIsLoading(false);
             } catch (error) {
@@ -26,22 +26,14 @@ function ProfilePosts(props) {
         };
     }, [username]);
 
-    if (isLoading)
-        return (
-            <div>
-                <LoadingDotsIcon />
-            </div>
-        );
+    if (isLoading) return <LoadingDotsIcon />;
 
     return (
         <div className="list-group">
-            {posts.map(post => {
-                const date = new Date(post.createdDate);
-                const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-
+            {posts.map((follower, index) => {
                 return (
-                    <Link key={post._id} to={`/post/${post._id}`} className="list-group-item list-group-item-action">
-                        <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong> <span className="text-muted small">on {dateFormatted} </span>
+                    <Link key={index} to={`/profile/${follower.username}`} className="list-group-item list-group-item-action">
+                        <img className="avatar-tiny" src={follower.avatar} /> {follower.username}
                     </Link>
                 );
             })}
@@ -49,4 +41,4 @@ function ProfilePosts(props) {
     );
 }
 
-export default ProfilePosts;
+export default ProfileFollowers;
